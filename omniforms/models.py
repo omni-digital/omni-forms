2df7a62df981fa2afbc1dd7b6fe22222cc8840fa
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-06-05 09:26:37 +0100

    Added OmniField models and associated tests

--- omniforms/models.py
+++ omniforms/models.py
@@ -4,9 +4,133 @@ Models for the omniforms app
 """
 from __future__ import unicode_literals
 from django import forms
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+import re
+
+
+@python_2_unicode_compatible
+class OmniField(models.Model):
+    """
+    Base class for omni fields
+    """
+    name = models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
+    help_text = models.TextField(blank=True, null=True)
+    required = models.BooleanField(default=False)
+    widget_class = models.CharField(max_length=255)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    form = GenericForeignKey()
+
+    class Meta(object):
+        """
+        Django properties
+        """
+        abstract = True
+
+    def __str__(self):
+        """
+        String representation of the instance
+
+        :return: Fields label
+        """
+        return self.label
+
+    @classmethod
+    def get_concrete_class_for_model_field(cls, model_field):
+        """
+        Method for getting a concrete model class to represent the type of form field required
+
+        :param model_field: Model Field instance
+        :type model_field: django.forms.Field
+
+        :return: OmniField subclass
+        """
+        return {
+            models.CharField: OmniCharField,
+            models.BooleanField: OmniBooleanField,
+            models.DateField: OmniDateField,
+            models.DateTimeField: OmniDateTimeField,
+            models.DecimalField: OmniDecimalField,
+            models.EmailField: OmniEmailField,
+            models.FloatField: OmniFloatField,
+            models.IntegerField: OmniIntegerField,
+            models.TimeField: OmniTimeField,
+            models.URLField: OmniUrlField
+        }.get(model_field.__class__)
+
+
+class OmniCharField(OmniField):
+    """
+    CharField representation
+    """
+    initial = models.TextField(blank=True, null=True)
+
+
+class OmniBooleanField(OmniField):
+    """
+    BooleanField representation
+    """
+    initial = models.NullBooleanField()
+
+
+class OmniDateField(OmniField):
+    """
+    DateField representation
+    """
+    initial = models.DateField(blank=True, null=True)
+
+
+class OmniDateTimeField(OmniField):
+    """
+    DateTimeField representation
+    """
+    initial = models.DateTimeField(blank=True, null=True)
+
+
+class OmniDecimalField(OmniField):
+    """
+    DecimalField representation
+    """
+    initial = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=10)
+
+
+class OmniEmailField(OmniField):
+    """
+    EmailField representation
+    """
+    initial = models.EmailField(blank=True, null=True)
+
+
+class OmniFloatField(OmniField):
+    """
+    FloatField representation
+    """
+    initial = models.FloatField(blank=True, null=True)
+
+
+class OmniIntegerField(OmniField):
+    """
+    IntegerField representation
+    """
+    initial = models.IntegerField(blank=True, null=True)
+
+
+class OmniTimeField(OmniField):
+    """
+    TimeField representation
+    """
+    initial = models.TimeField(blank=True, null=True)
+
+
+class OmniUrlField(OmniField):
+    """
+    IntegerField representation
+    """
+    initial = models.URLField(blank=True, null=True)
 
 
 @python_2_unicode_compatible
@@ -38,7 +162,7 @@ class OmniFormBase(models.Model):
         """
         return str('{0}{1}'.format(
             self.__class__.__name__,
-            self.title.capitalize()
+            ''.join([fragment.capitalize() for fragment in re.split('\W+', self.title)])
         ))
 
     @staticmethod
--- omniforms/tests/test_models.py
+++ omniforms/tests/test_models.py
@@ -4,10 +4,27 @@ Tests the omniforms models
 """
 from __future__ import unicode_literals
 from django import forms
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.test import TestCase
-from omniforms.models import OmniFormBase, OmniModelFormBase, OmniForm, OmniModelForm
+from omniforms.models import (
+    OmniFormBase,
+    OmniModelFormBase,
+    OmniForm,
+    OmniModelForm,
+    OmniField,
+    OmniCharField,
+    OmniBooleanField,
+    OmniDateField,
+    OmniDateTimeField,
+    OmniDecimalField,
+    OmniEmailField,
+    OmniFloatField,
+    OmniIntegerField,
+    OmniTimeField,
+    OmniUrlField,
+)
 from omniforms.tests.models import DummyModel
 
 
@@ -66,13 +83,13 @@ class OmniFormTestCase(TestCase):
     """
     def setUp(self):
         super(OmniFormTestCase, self).setUp()
-        self.omniform = OmniForm.objects.create(title='test')
+        self.omniform = OmniForm.objects.create(title='test model')
 
     def test_get_form_class_name(self):
         """
         The _get_form_class_name method should return an appropriate name for the form
         """
-        self.assertEqual('OmniFormTest', self.omniform._get_form_class_name())
+        self.assertEqual('OmniFormTestModel', self.omniform._get_form_class_name())
 
     def test_get_form_class_bases(self):
         """
@@ -130,3 +147,310 @@ class OmniModelFormTestCase(TestCase):
         meta_class = self.omniform._get_form_meta_class()
         self.assertEqual(meta_class.__name__, 'Meta')
         self.assertEqual(meta_class.model, DummyModel)
+
+
+class OmniFieldTestCase(TestCase):
+    """
+    Tests the OmniField model
+    """
+    def test_name_field(self):
+        """
+        The model should have a name field
+        """
+        field = OmniField._meta.get_field('name')
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 255)
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
+
+    def test_label_field(self):
+        """
+        The model should have a label field
+        """
+        field = OmniField._meta.get_field('label')
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 255)
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
+
+    def test_help_text_field(self):
+        """
+        The model should have a help_text field
+        """
+        field = OmniField._meta.get_field('help_text')
+        self.assertIsInstance(field, models.TextField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+    def test_required_field(self):
+        """
+        The model should have a required field
+        """
+        field = OmniField._meta.get_field('required')
+        self.assertIsInstance(field, models.BooleanField)
+        self.assertFalse(field.default)
+
+    def test_widget_class_field(self):
+        """
+        The model should have a widget_class field
+        """
+        field = OmniField._meta.get_field('widget_class')
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 255)
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
+
+    def test_content_type_field(self):
+        """
+        The model should have a content_type field
+        """
+        field = OmniField._meta.get_field('content_type')
+        self.assertIsInstance(field, models.ForeignKey)
+        self.assertEqual(field.rel.to, ContentType)
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
+
+    def test_object_id_field(self):
+        """
+        The model should have a object_id field
+        """
+        field = OmniField._meta.get_field('object_id')
+        self.assertIsInstance(field, models.PositiveIntegerField)
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
+
+    def test_form_field(self):
+        """
+        The model should have a form field
+        """
+        field = OmniField.form
+        self.assertIsInstance(field, GenericForeignKey)
+
+    def test_string_representation(self):
+        """
+        The forms label should be used as the string representation
+        """
+        instance = OmniField(label='Do you like cats?')
+        self.assertEqual('{0}'.format(instance), instance.label)
+
+    def test_is_abstract(self):
+        """
+        The model class should be abstract
+        """
+        self.assertTrue(OmniField._meta.abstract)
+
+    def test_get_concrete_class_for_model_field(self):
+        """
+        The get_concrete_class_for_model_field method should return the correct OmniField subclass
+        """
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.CharField()), OmniCharField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.BooleanField()), OmniBooleanField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.DateTimeField()), OmniDateTimeField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.DecimalField()), OmniDecimalField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.EmailField()), OmniEmailField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.FloatField()), OmniFloatField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.IntegerField()), OmniIntegerField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.TimeField()), OmniTimeField)
+        self.assertEqual(OmniField.get_concrete_class_for_model_field(models.URLField()), OmniUrlField)
+
+
+class OmniCharFieldTestCase(TestCase):
+    """
+    Tests the OmniCharField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniCharField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniCharField._meta.get_field('initial')
+        self.assertIsInstance(field, models.TextField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniBooleanFieldTestCase(TestCase):
+    """
+    Tests the OmniBooleanField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniBooleanField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniBooleanField._meta.get_field('initial')
+        self.assertIsInstance(field, models.NullBooleanField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniEmailFieldTestCase(TestCase):
+    """
+    Tests the OmniEmailField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniEmailField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniEmailField._meta.get_field('initial')
+        self.assertIsInstance(field, models.EmailField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniDateFieldTestCase(TestCase):
+    """
+    Tests the OmniDateField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniDateField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniDateField._meta.get_field('initial')
+        self.assertIsInstance(field, models.DateField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniDateTimeFieldTestCase(TestCase):
+    """
+    Tests the OmniDateTimeField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniDateTimeField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniDateTimeField._meta.get_field('initial')
+        self.assertIsInstance(field, models.DateTimeField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniDecimalFieldTestCase(TestCase):
+    """
+    Tests the OmniDecimalField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniDecimalField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniDecimalField._meta.get_field('initial')
+        self.assertIsInstance(field, models.DecimalField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+        self.assertEqual(field.decimal_places, 2)
+        self.assertEqual(field.max_digits, 10)
+
+
+class OmniFloatFieldTestCase(TestCase):
+    """
+    Tests the OmniFloatField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniFloatField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniFloatField._meta.get_field('initial')
+        self.assertIsInstance(field, models.FloatField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniIntegerFieldTestCase(TestCase):
+    """
+    Tests the OmniIntegerField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniIntegerField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniIntegerField._meta.get_field('initial')
+        self.assertIsInstance(field, models.IntegerField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniTimeFieldTestCase(TestCase):
+    """
+    Tests the OmniTimeField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniTimeField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniTimeField._meta.get_field('initial')
+        self.assertIsInstance(field, models.TimeField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
+
+
+class OmniUrlFieldTestCase(TestCase):
+    """
+    Tests the OmniUrlField
+    """
+    def test_subclasses_omni_field(self):
+        """
+        The model should subclass OmniField
+        """
+        self.assertTrue(issubclass(OmniUrlField, OmniField))
+
+    def test_initial(self):
+        """
+        The model should have an initial field
+        """
+        field = OmniUrlField._meta.get_field('initial')
+        self.assertIsInstance(field, models.URLField)
+        self.assertTrue(field.blank)
+        self.assertTrue(field.null)
