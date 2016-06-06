6cbf62c7c0630d688b6eab5c9876169e81bcecd5
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-06-06 11:26:12 +0100

    Added new test stub with authenticated admin user

--- omniforms/tests/utils.py
+++ omniforms/tests/utils.py
@@ -3,6 +3,7 @@
 Test stubs for the omniforms app
 """
 from __future__ import unicode_literals
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from omniforms.tests.factories import OmniModelFormFactory
 
@@ -14,3 +15,16 @@ class OmniFormTestCaseStub(TestCase):
     def setUp(self):
         super(OmniFormTestCaseStub, self).setUp()
         self.omni_form = OmniModelFormFactory.create()
+
+
+class OmniFormAdminTestCaseStub(OmniFormTestCaseStub):
+    """
+    Test Stub for the omniforms app
+    """
+    def setUp(self):
+        super(OmniFormAdminTestCaseStub, self).setUp()
+        user_model = get_user_model()
+        self.user = user_model(username='test', email='test@example.com', is_staff=True)
+        self.user.set_password('test1234')
+        self.user.save()
+        self.client.login(username='test', password='test1234')
