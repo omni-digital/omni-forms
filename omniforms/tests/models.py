954568add04862c8f6df2b7c46e9f418c1169155
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-06-06 11:25:46 +0100

    Added extra fields to test model

--- omniforms/tests/models.py
+++ omniforms/tests/models.py
@@ -6,3 +6,12 @@ class DummyModel(models.Model):
     Dummy Model for use with tests
     """
     title = models.CharField(max_length=255)
+    agree = models.BooleanField()
+    some_date = models.DateField()
+    some_datetime = models.DateTimeField()
+    some_decimal = models.DecimalField(decimal_places=10, max_digits=12)
+    some_email = models.EmailField()
+    some_float = models.FloatField()
+    some_integer = models.IntegerField()
+    some_time = models.TimeField()
+    some_url = models.URLField()
