ddb404fe7495a7837b1bf912814d96e839336542
Dan Stringer <dan.stringer1983@gmail.com>
2016-12-02 14:50:43 +0000

    Updated setup scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-
+# -*- coding: utf-8 -*-
+"""
+Setup scripts for omniforms
+"""
 from setuptools import find_packages, setup
 import omniforms
 import os
 
 
-# with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-#     README = readme.read()
+# Read the contents of the README rst file for the long description
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
 
 # allow setup.py to be run from any path
@@ -17,25 +21,30 @@ setup(
     packages=find_packages(exclude=['app']),
     include_package_data=True,
     license='MIT',
-    description='A simple Django app to build model forms using the admin interface.',
-    long_description='',
-    url='https://www.example.com/',
-    author='Your Name',
+    description='A simple Django app to build forms using the admin interface.',
+    long_description=README,
+    url='https://github.com/omni-digital/omni-forms/',
+    download_url='https://github.com/omni-digital/omni-forms/tarball/{0}'.format(omniforms.get_version()),
+    author='Omni Digital',
     author_email='yourname@example.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # example license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    keywords=['csv generator', 'queryset', 'django'],
     install_requires=['django-braces==1.9.0']
 )
